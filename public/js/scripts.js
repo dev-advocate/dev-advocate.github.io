b2835b5c0e0d7ebf89b8d99852e635fc0daec367
David Mello <david@dev-advocate.com>
2019-09-10 18:07:23 -0400

    css changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', function (event) {
     
 	// If the clicked element doesn't have the right selector, bail
-	if (!event.target.matches('.js--open-modal')) {
+	if (!event.target.matches('.js--open-employee-modal')) {
         return;
     }
 
@@ -23,13 +23,40 @@ document.addEventListener('click', function (event) {
         }
     });
 
-    modal.setContent('<div id="model-inner"></div>');
+    modal.setContent('<iframe src="https://services.cognitoforms.com/f/GhIb2C-hDUWSeIR5xktX6w?id=1" style="position:relative;width:1px;min-width:100%;*width:100%;" frameborder="0" scrolling="yes" seamless="seamless" height="764" width="100%"></iframe><script src="https://services.cognitoforms.com/scripts/embed.js"></script>');
 
-    hbspt.forms.create({
-        portalId: '6396830',
-        formId: '2136d2b7-a4fd-4f14-8c54-be1e66712e94',
-        target: '#model-inner'
+
+    modal.open();
+
+}, false);
+
+document.addEventListener('click', function (event) {
+    
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.js--open-employer-modal')) {
+        return;
+    }
+
+	// Don't follow the link
+	event.preventDefault();
+
+	// instanciate new modal
+    var modal = new tingle.modal({
+        footer: false,
+        closeMethods: [ 'button', 'escape'],
+        closeLabel: "Close",
+        cssClass: ['custom-class-1', 'custom-class-2'],
+        onOpen: function() {
+        },
+        onClose: function() {
+        },
+        beforeClose: function() {
+            return true; // close the modal
+        }
     });
+
+    modal.setContent('<iframe src="https://services.cognitoforms.com/f/GhIb2C-hDUWSeIR5xktX6w?id=2" style="position:relative;width:1px;min-width:100%;*width:100%;" frameborder="0" scrolling="yes" seamless="seamless" height="616" width="100%"></iframe><script src="https://services.cognitoforms.com/scripts/embed.js"></script>');
+
 
     modal.open();
 
